0ae07dbed118584ae3c22e6050df3c946035cc4a
Baalaji <baalajimaestro@pixelexperience.org>
2019-12-30 12:21:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,6 +15,6 @@ OUT="bin/targets/ipq40xx/generic/"
 FILE_NAME="openwrt-ipq40xx-generic-asus_rt-ac58u-squashfs-sysupgrade"
 NEW_FILE_NAME="${FILE_NAME}-$(date +%d%m%Y).bin"
 mv "${OUT}${FILE_NAME}.bin" "${OUT}${NEW_FILE_NAME}"
-FILE_PATH="${OUT}${FILE_NAME}"
+FILE_PATH="${OUT}${NEW_FILE_NAME}"
 scp  -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $FILE_PATH baalaji20@storage.osdn.net:/storage/groups/b/ba/baalajimaestrobuilds/openwrt-snapshots/$NEW_FILE_NAME
 echo "Pushed to OSDN!"
