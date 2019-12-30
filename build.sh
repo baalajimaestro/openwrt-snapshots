c48f2b8de0a3a8acf761726ad481141851f022a1
baalajimaestro <baalajimaestro@pixelexperience.org>
2019-12-30 08:31:14 +0530

    Fix script derp
    Signed-off-by: baalajimaestro <baalajimaestro@pixelexperience.org>

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
-mkdir -p /home/runner/.ssh
-curl -sL -u baalajimaestro:$GH_PERSONAL_TOKEN -o /home/runner/.ssh/id_ed25519 https://raw.githubusercontent.com/baalajimaestro/keys/master/id_ed25519
+mkdir -p /home/ci/.ssh
+curl -sL -u baalajimaestro:$GH_PERSONAL_TOKEN -o /home/ci/.ssh/id_ed25519 https://raw.githubusercontent.com/baalajimaestro/keys/master/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
 echo "SSH Keys Set!"
 git clone https://github.com/openwrt/openwrt
+while true; do echo "Building OpenWRT....."; sleep 120; done &
 cd openwrt
 ./scripts/feeds update -a &>> /dev/null
 ./scripts/feeds install -a &>> /dev/null
 cp ../openwrt_config .config
-make -j$(nproc) download world & while true; do echo "Building OpenWRT....."; sleep 120; done
+make -j$(nproc) download world
+jobs
+kill %1
 i=$(find . | grep "sysupgrade")
 echo $i
 scp  -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $i baalaji20@storage.osdn.net:/storage/groups/b/ba/baalajimaestrobuilds/openwrt-snapshots/$i
